b91d51d89d2d0326ca281423872628c9686e5066
George Murphy <george.moguls@gmail.com>
2022-08-07 20:39:48 +1000

    added seeds.sql and schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,19 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(8,2) NOT NULL,
   department_id INT NOT NULL,
-  FOREIGN KEY (deparment_id)
+  FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
@@ -18,10 +23,10 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT,
   FOREIGN KEY (role_id)
-  REFERENCES roles(id)
+  REFERENCES role(id)
   ON DELETE SET NULL,
+  manager_id INT,
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
   ON DELETE SET NULL,
