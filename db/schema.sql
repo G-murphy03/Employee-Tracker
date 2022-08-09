ebebf4de3ab433e55ad37fd506f13a2c7761c11f
George Murphy <george.moguls@gmail.com>
2022-08-09 11:20:33 +1000

    ASK BCS

--- db/schema.sql
+++ db/schema.sql
@@ -8,17 +8,17 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(8,2) NOT NULL,
-  department_id INT NOT NULL,
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
 const fs  = require('fs');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'employee_tracker'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  afterConnection();
+});
+
+// function after connection is established and welcome image shows 
+afterConnection = () => {
+  console.log("***********************************")
+  console.log("*                                 *")
+  console.log("*        EMPLOYEE MANAGER         *")
+  console.log("*                                 *")
+  console.log("***********************************")
+  init();
+};
 
 function init() {
     inquirer.prompt(
@@ -12,12 +36,42 @@ function init() {
             }
         ])
         .then((answer) => {
-            if(answer===choices[0]) {
-                
-            } else if(answer===choices[1]){
+            const { options } = answer; 
+            if(options==='View All Employees') {
+                viewAllEmployees();
+            } else if(options==='Add Employee'){
 
-            } else if (answer===choices[2]){
+            } else if (options==='Update Employee Role'){
 
+            } else if (options==='View All Roles'){
+
+            } else if (options==='Add Role'){
+
+            } else if (options==='View All Departments'){
+
+            } else if (options==='Add Department'){
+
+            } else {
+                connection.end();
             }
         })
 }
+
+const viewAllEmployees = () => {
+    console.log('Showing all employees...\n'); 
+    const sql = `SELECT employee.id, 
+                      employee.first_name, 
+                      employee.last_name, 
+                      role.title, 
+                      department.name AS department,
+                      role.salary, 
+                      CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee`;
+    connection.promise().query(sql)
+    .then( (rows) => {
+    console.table(rows);
+    init();
+    })
+    .catch(console.error('error'))
+    .then( () => connection.end());
+};
+
